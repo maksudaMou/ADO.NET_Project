f5a756ffccb3e9a5f819e5e96f1df83ff060c91d
Maksuda Yesmin <106536736+maksudaMou@users.noreply.github.com>
2022-09-28 00:48:47 +0600

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,17 @@
+﻿Create table Vehicles
+(
+	VehicleId INT PRIMARY KEY,
+	VehicleType NVARCHAR(40) NOT NULL,
+	Model NVARCHAR(40) NOT NULL,
+	[Year] INT NOT NULL,
+	picture NVARCHAR(150) NOT NULL
+)
+GO
+Create table Repairs
+(
+	RepairId INT PRIMARY KEY,
+	Problem NVARCHAR(150) NOT NULL,
+	VehicleId INT NOT NULL REFERENCES Vehicles(VehicleId),
+	Cost MONEY NOT NULL
+)
+go
